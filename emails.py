82cd95f75bfd29bab4e9faf2330faec6869ff42e
Andrew Pruitt <apruitt0321@gmail.com>
2019-05-14 12:51:10 -0500

    Added ability to specify an output file.

--- emails.py
+++ emails.py
@@ -6,16 +6,17 @@ import argparse
 start_date = None
 end_date = None
 input_file = 'subs.csv'
-month = 'april2019'
-filename = f'cure_emails/{month}_subs.csv'
+output_file = None
 subbed = None
 
 # Add CLI arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--date', 
         help="Enter a date, ex: 'July 2012'.")
-parser.add_argument('-f', '--file',
-        help="Specify a csv file to examine.")
+parser.add_argument('-if', '--in_file',
+        help="Specify the path to a csv file as input.")
+parser.add_argument('-of', '--out_file',
+        help="Specify the path to an output file.")
 args = parser.parse_args()
 
 # Parses date argument. If no date argument is provided, defaults to current
@@ -42,9 +43,11 @@ else:
         y += 1
     end_date = pd.to_datetime(f"{y}-{m+1}")
 
-# Parses file arguments. Defaults to 'subs.csv'.
-if args.file:
-    input_file = args.file
+# Parses file arguments.
+if args.in_file:
+    input_file = args.in_file
+if args.out_file:
+    output_file = args.out_file
 
 # Open and read csv into pandas dataframe object
 with open(input_file,'r') as f:
@@ -54,7 +57,9 @@ with open(input_file,'r') as f:
 subbed['Atime'] = pd.to_datetime(subbed['AddedTime'])
 subbed['Rtime'] = pd.to_datetime(subbed['RemovedTime'],errors='ignore')
 
-# Filter dataframe.
+# Filter dataframe. Takes slice of all entries subscribed before end_date,
+# then returns slice of those who are still subscribed or whose unsubscription
+# date is after end_date.
 subs_bfr = subbed.loc[(subbed['Atime'] < end_date)]
 subs_btwn = subs_bfr.loc[(subs_bfr['SubscriberStatus'] == 'Subscribed')
         | (subs_bfr['Rtime'] > end_date)]
@@ -67,5 +72,7 @@ print(start_date)
 print(end_date)
 
 # Generate report by writing resutls to file
-#with open(filename, 'w') as f:
-#    totalsubs.to_csv(f)
+# Need to remove 'AddedTime' and 'RemovedTime' in favor of 'Atime' and 'Rtime'
+output_file = f"{start_date.year}-{start_date.month}.csv"
+#with open(output_file, 'w') as f:
+#    subs_btwn.to_csv(f)
