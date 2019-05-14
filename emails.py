77f85615f720664010725410ebdd051222337776
Andrew Pruitt <apruitt0321@gmail.com>
2019-05-14 14:14:59 -0500

    Added gen_report function to print a summary and save info to file quietly.

--- emails.py
+++ emails.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import pandas as pd
 import argparse
 
@@ -8,6 +8,15 @@ end_date = None
 input_file = 'subs.csv'
 output_file = None
 subbed = None
+
+def gen_report(df, of):
+    r = ['Email Address', 'FirstName', 'LastName', 
+        'SubscriberStatus', 'AddedTime', 'RemovedTime']
+    with open(of, 'w') as f:
+        df.to_csv(f)
+    rows = df[r].shape[0]
+    print(f"Full data written to {of}.")
+    print(f"There were {rows} total entries.")
 
 # Add CLI arguments
 parser = argparse.ArgumentParser()
@@ -19,7 +28,7 @@ parser.add_argument('-of', '--out_file',
         help="Specify the path to an output file.")
 args = parser.parse_args()
 
-# Parses date argument. If no date argument is provided, defaults to current
+# Parse date argument. If no date argument is provided, defaults to current
 # month and year.
 if args.date:
     try:
@@ -28,7 +37,7 @@ if args.date:
         y = start_date.year
         if m == 12:
             m = 0
-            y = start_date.year + 1
+            y += 1
         end_date = pd.to_datetime(f"{y}-{m+1}")
     except ValueError: 
         print("Could not parse date. Use -h for help.")
@@ -43,36 +52,37 @@ else:
         y += 1
     end_date = pd.to_datetime(f"{y}-{m+1}")
 
-# Parses file arguments.
+# Parse file arguments.
 if args.in_file:
     input_file = args.in_file
 if args.out_file:
     output_file = args.out_file
+else:
+    output_file = f"{args.date}.csv"
 
 # Open and read csv into pandas dataframe object
+print("Loading csv file into dataframe...")
 with open(input_file,'r') as f:
     subbed = pd.read_csv(f, delimiter=',')
+print("Done.")
 
 # Convert times to datetime objects. Time intesive.
-subbed['Atime'] = pd.to_datetime(subbed['AddedTime'])
-subbed['Rtime'] = pd.to_datetime(subbed['RemovedTime'],errors='ignore')
+print("Converting 'AddedTime'...")
+subbed['AddedTime'] = pd.to_datetime(subbed['AddedTime'])
+print("Done.")
+print("Converting 'RemovedTime'...")
+subbed['RemovedTime'] = pd.to_datetime(subbed['RemovedTime'],errors='ignore')
+print("Done.")
 
 # Filter dataframe. Takes slice of all entries subscribed before end_date,
 # then returns slice of those who are still subscribed or whose unsubscription
 # date is after end_date.
-subs_bfr = subbed.loc[(subbed['Atime'] < end_date)]
+subs_bfr = subbed.loc[(subbed['AddedTime'] < end_date)]
 subs_btwn = subs_bfr.loc[(subs_bfr['SubscriberStatus'] == 'Subscribed')
-        | (subs_bfr['Rtime'] > end_date)]
-
-# Print filtered results. Mostly for debugging. 
-r = ['Email Address', 'FirstName', 'LastName', 
-        'SubscriberStatus', 'Atime', 'Rtime']
-print(subs_btwn[r])
-print(start_date)
-print(end_date)
+        | (subs_bfr['RemovedTime'] > end_date)]
 
 # Generate report by writing resutls to file
 # Need to remove 'AddedTime' and 'RemovedTime' in favor of 'Atime' and 'Rtime'
-output_file = f"{start_date.year}-{start_date.month}.csv"
-#with open(output_file, 'w') as f:
-#    subs_btwn.to_csv(f)
+gen_report(subs_btwn, output_file)
+
+
