fa9c9bf4ff6344a8d9d808d65101e5d06b8ba5c4
Andrew Pruitt <apruitt0321@gmail.com>
2019-05-14 11:14:35 -0500

    Accounted for empty 'RemovedTime' cells.

--- emails.py
+++ emails.py
@@ -3,21 +3,40 @@ import pandas as pd
 import argparse
 import datetime as dt
 
+date = None
+m_incr = { 'january': 'february',
+            'february': 'march',
+            'march': 'april',
+            'april': 'may',
+            'may': 'june',
+            'june': 'july',
+            'july': 'august',
+            'august': 'september',
+            'september': 'october',
+            'october': 'november',
+            'november': 'december',
+            'december': 'january'}
+
 # Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--date', 
         help="Enter a date, ex: 'July 2012'.")
 parser.add_argument('-f', '--file',
         help="Specify a csv file to examine.")
-date = None
 args = parser.parse_args()
 if args.date:
-    try: date = pd.to_datetime(args.date)
+    try:
+        adate = args.date.lower().split(" ")
+        if adate[0] == 'december':
+            adate[1] = str(int(adate[1]) + 1)
+        adate[0] = m_incr[adate[0]]
+        date = pd.to_datetime(adate[0]+" "+adate[1])
     except ValueError: 
         print("Incorrect date format. Use -h for help.")
         raise
 else:
     date = pd.to_datetime('now')
+print(date)
 
 if args.file:
     input_file = args.file
@@ -25,31 +44,28 @@ else:
     input_file = 'subs.csv'
 
 # Declare variables
-start_date = '2018-10-01'
-end_date = '2018-10-31'
+start_date = '2019-04-01'
+end_date = '2019-05-01'
 month = 'april2019'
 filename = f'cure_emails/{month}_subs.csv'
-
 # Open and read csv into pandas dataframe object
 subbed = None
 with open(input_file,'r') as f:
     subbed = pd.read_csv(f, delimiter=',')
 
-#subbed['pAddTime'] = pd.to_datetime(subbed['AddedTime'],errors='coerce')
-#subbed['pRemoveTime'] = pd.to_datetime(subbed['RemovedTime'],errors='coerce')
+# Convert times to datetime objects. Time intesive.
+#subbed['pAtime'] = pd.to_datetime(subbed['AddedTime'])
+#subbed['pRtime'] = pd.to_datetime(subbed['RemovedTime'],errors='ignore')
 
-# Filter dataframe
-subs = subbed.loc[(subbed['AddedTime'] < end_date) 
-        & (subbed['RemovedTime'] > end_date)] 
-cleaned = subbed.loc[(subbed['SubscriberStatus'] == 'Cleaned') 
-        & (subbed['AddedTime'] >= start_date) 
-        & (subbed['RemovedTime'] <= end_date)]
-cleanEmails = cleaned[['Email Address']]
-totalsubs = subs.loc[~subs['Email Address'].isin(cleanEmails)].reset_index()
+# Filter dataframe.
+#subs_bfr = subbed.loc[(subbed['pAtime'] < pd.to_datetime(end_date))]
+#subs_btwn = subs_bfr.loc[(subs_bfr['SubscriberStatus'] == 'Subscribed')
+#        | (subs_bfr['pRtime'] > pd.to_datetime(end_date))]
 
 # Print filtered results
-print(totalsubs[['Email Address', 'AddedTime',
-    'RemovedTime', 'SubscriberStatus']])
+r = ['Email Address', 'FirstName', 'LastName', 
+        'SubscriberStatus', 'pAtime', 'pRtime']
+#print(subs_btwn[r])
 
 # Write resutls to file
 #with open(filename, 'w') as f:
