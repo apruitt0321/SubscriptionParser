380e3e4c413e22f1f444a88d198087bb37d02cf9
Andrew Pruitt <apruitt0321@gmail.com>
2019-05-14 11:38:42 -0500

    CLI interface working. Can specify desired month via the '-d' flag.

--- emails.py
+++ emails.py
@@ -3,7 +3,9 @@ import pandas as pd
 import argparse
 import datetime as dt
 
-date = None
+# Variables
+start_date = None
+end_date = None
 m_incr = { 'january': 'february',
             'february': 'march',
             'march': 'april',
@@ -16,8 +18,12 @@ m_incr = { 'january': 'february',
             'october': 'november',
             'november': 'december',
             'december': 'january'}
+input_file = 'subs.csv'
+month = 'april2019'
+filename = f'cure_emails/{month}_subs.csv'
+subbed = None
 
-# Arguments
+# Handle CLI arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--date', 
         help="Enter a date, ex: 'July 2012'.")
@@ -27,46 +33,41 @@ args = parser.parse_args()
 if args.date:
     try:
         adate = args.date.lower().split(" ")
+        start_date = pd.to_datetime(adate[0]+" "+adate[1])
         if adate[0] == 'december':
             adate[1] = str(int(adate[1]) + 1)
         adate[0] = m_incr[adate[0]]
-        date = pd.to_datetime(adate[0]+" "+adate[1])
+        end_date = pd.to_datetime(adate[0]+" "+adate[1])
     except ValueError: 
-        print("Incorrect date format. Use -h for help.")
+        print("Could not parse date. Use -h for help.")
         raise
 else:
-    date = pd.to_datetime('now')
-print(date)
-
+    print("Date not specified. Defaulting to current month...")
+    m = pd.to_datetime('today').month
+    y = pd.to_datetime('today').year
+    start_date = pd.to_datetime(f"{y}-{m}")
+    end_date = pd.to_datetime(f"{y}-{m+1}")
 if args.file:
     input_file = args.file
-else:
-    input_file = 'subs.csv'
 
-# Declare variables
-start_date = '2019-04-01'
-end_date = '2019-05-01'
-month = 'april2019'
-filename = f'cure_emails/{month}_subs.csv'
 # Open and read csv into pandas dataframe object
-subbed = None
 with open(input_file,'r') as f:
     subbed = pd.read_csv(f, delimiter=',')
 
 # Convert times to datetime objects. Time intesive.
-#subbed['pAtime'] = pd.to_datetime(subbed['AddedTime'])
-#subbed['pRtime'] = pd.to_datetime(subbed['RemovedTime'],errors='ignore')
+subbed['pAtime'] = pd.to_datetime(subbed['AddedTime'])
+subbed['pRtime'] = pd.to_datetime(subbed['RemovedTime'],errors='ignore')
 
 # Filter dataframe.
-#subs_bfr = subbed.loc[(subbed['pAtime'] < pd.to_datetime(end_date))]
-#subs_btwn = subs_bfr.loc[(subs_bfr['SubscriberStatus'] == 'Subscribed')
-#        | (subs_bfr['pRtime'] > pd.to_datetime(end_date))]
+subs_bfr = subbed.loc[(subbed['pAtime'] < pd.to_datetime(end_date))]
+subs_btwn = subs_bfr.loc[(subs_bfr['SubscriberStatus'] == 'Subscribed')
+        | (subs_bfr['pRtime'] > pd.to_datetime(end_date))]
 
-# Print filtered results
+# Print filtered results. Mostly for debugging. 
 r = ['Email Address', 'FirstName', 'LastName', 
         'SubscriberStatus', 'pAtime', 'pRtime']
-#print(subs_btwn[r])
+print(subs_btwn[r])
 
-# Write resutls to file
+# Generate report by writing resutls to file
 #with open(filename, 'w') as f:
 #    totalsubs.to_csv(f)
