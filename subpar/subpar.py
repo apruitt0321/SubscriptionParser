c0579981900ec53476df0ef26033ff98e7c77ca3
Andrew Pruitt <apruitt0321@gmail.com>
2019-05-23 16:21:39 -0500

    Path normalization. Attempts to modify graph output resolution are so far unsuccessful.

--- subpar/subpar.py
+++ subpar/subpar.py
@@ -2,12 +2,13 @@
 import pandas as pd
 import argparse
 import matplotlib.pyplot as plt
+import os.path as path
 
 # Variables
 info = None                     #First day of desired month
 start_date = pd.to_datetime('2018-05-01')  # Earliest supported date
 end_date = None                 # First day of the month after desired
-input_file = 'subs.csv'        # csv file to read
+input_file = path.normpath('./subs.csv')        # csv file to read
 output_file = None              # csv file to save to
 ytd_df = None
 
@@ -36,16 +37,13 @@ else:
     info = pd.to_datetime(f"{y}-{m}")
     end_date = info + pd.DateOffset(months=1)
 
-# Parse file arguments.
 if args.in_file:
-    input_file = args.in_file
+    input_file = path.normpath(args.in_file)
 if args.out_file:
-    output_file = args.out_file
+    output_file = path.normpath(args.out_file)
 else:
-    output_file = f"../Reports/{info.month}-{info.year}.csv"
+    output_file = path.normpath(f"../Reports/{info.month}-{info.year}.csv")
 
-# Open and read csv into pandas dataframe object
-# Convert times to datetime objects.
 def load_df(infile):
     s = None
     with open(infile,'r') as f:
@@ -56,24 +54,24 @@ def load_df(infile):
             format = '%m/%d/%Y %H:%M', errors='ignore')
     return s
 
-# Filter dataframe.
 def filter_time(subbed, ed=end_date):
     subs_bfr = subbed.loc[(subbed['AddedTime'] < ed)]
     subs_btwn = subs_bfr.loc[(subs_bfr['SubscriberStatus'] == 'Subscribed')
             | (subs_bfr['RemovedTime'] > ed)]
     return subs_btwn
 
-def plot_year(subbed):
+def get_year(subbed):
     s = []
-    for i in range(1,13):
-        d = start_date+pd.DateOffset(months=i)
+    m = start_date
+    for i in range(0,13):
+        m = start_date + pd.DateOffset(months=i)
+        d = start_date + pd.DateOffset(months=(i+1))
         x = filter_time(subbed, d)
-        key = f"{d.month}/{d.year}"
+        key = f"{m.month}/{m.year}"
         s.append([key, x.shape[0]])
     subs_over_time = pd.DataFrame(s, columns=['Month', 'Subscribers'])
     return subs_over_time
 
-# Function to write pandas dataframe to a csv file
 def gen_report(df, of):
     with open(of, 'w') as f:
         df.to_csv(f)
@@ -84,18 +82,12 @@ def gen_report(df, of):
 def run():
     subbed = load_df(input_file)
     subs_btwn = filter_time(subbed)
-    subs_over_time = plot_year(subbed)
-    subs_over_time.plot(x='Month', y = 'Subscribers')
-#    plt.ylim(ymin=12000)           # for bar graphs
-    plt.savefig('../Reports/graph.png')
+    subs_over_time = get_year(subbed)
+    print(subs_over_time)
+    p = subs_over_time.set_index('Month').plot(grid=True)
+    plt.savefig(path.normpath('../Reports/graph.png'))
     gen_report(subs_btwn, output_file)
 
 if __name__ == "__main__":
-    subbed = load_df(input_file)
-    subs_btwn = filter_time(subbed)
-    subs_over_time = plot_year(subbed)
-    subs_over_time.plot(x='Month', y = 'Subscribers')
-    plt.savefig('../Reports/graph.png')
-    gen_report(subs_btwn, output_file)
-
+    run()
 
